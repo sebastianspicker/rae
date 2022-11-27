228a84d666cc071e387bb754bb3b8dc4588cf91f
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-11-27 22:43:00 +0100

    test(orchestration): cover the first usable orchestration flow

---
+++ scripts/orchestration-notes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+run_orchestration() {
+  printf '%s\n' 'orchestration ready'
+}
+
+# current lane: orchestration
+run_orchestration() {
+  printf '%s\n' 'orchestration ready'
+}
