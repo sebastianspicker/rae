c0f9aa7f57407c4bb587e67be3e43b296351f744
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-04-30 17:14:00 +0200

    test(typescript): cover the first usable typescript flow in the orchestration lane

---
+++ scripts/typescript-cleanup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+run_typescript() {
+  printf '%s\n' 'typescript ready'
+}
+
+# current lane: typescript
+run_typescript() {
+  printf '%s\n' 'typescript ready'
+}
